5eaa80a4734f05bdac71375afef45a172c2974b0
Ruslan Kazankov <kazankovruslankrk@gmail.com>
2024-10-11 18:48:06 +0800

    [CSJHW-6]: Add migrations of task_comments

---
+++ src/HomeworkApp.Dal/Migrations/20241011181800_AddModifiedAtAndDeletedAt_InTaskComments.cs
@@ -0,0 +1,28 @@
+﻿using FluentMigrator;
+
+namespace HomeworkApp.Dal.Migrations;
+
+[Migration(20241011181800, TransactionBehavior.None)]
+public class AddModifiedAtAndDeletedAt_InTaskComments : Migration
+{
+    public override void Up()
+    {
+        const string sql = @"
+ALTER TABLE  task_comments
+  ADD COLUMN modified_at timestamp with time zone NULL,
+  ADD COLUMN deleted_at timestamp with time zone NULL;
+        ";
+
+        Execute.Sql(sql);
+    }
+
+    public override void Down()
+    {
+        const string sql = @"
+ALTER TABLE  task_comments
+ DROP COLUMN modified_at,
+ DROP COLUMN deleted_at;
+        ";
+        Execute.Sql(sql);
+    }
+}
